eacfee73e961705c678998711d7e04f5b27a76c0
simonholmes001 <simonholmesabc@gmail.com>
2020-06-07 09:27:53 +0200

    test control for BatchData

---
+++ attention/test_1.py
@@ -0,0 +1,22 @@
+from batch_data import BatchData
+
+input_path = '/home/simon/mathisi2/attention_proteins/data'
+batch_size = 1
+n_workers = 1
+validation_split = 0.2
+shuffle_dataset = True
+
+batch = BatchData(input_path, batch_size, n_workers, validation_split, shuffle_dataset)
+batch.dataset()
+x,y = batch.batch_data()
+
+print('=' * 83)
+dataiter = iter(x)
+data = dataiter.next()
+features, labels = data
+print(f"features: \n{features}\n"
+      f"labels: \n{labels}\n"
+      f"len features: {len(features)}\n"
+      f"len labels: {len(labels)}\n")
+print('=' * 83)
+
