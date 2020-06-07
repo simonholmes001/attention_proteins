020f10db47803399455f63b91a8ab0349085c57f
simonholmes001 <simonholmesabc@gmail.com>
2020-06-07 09:26:24 +0200

    create model

--- attention/attention.py
+++ attention/attention.py
@@ -1,42 +1,87 @@
-import os
-
-import torch
-from torch.utils.data import Dataset, DataLoader
-import pickle
 import math
-
-from protein_loader import ProteinLoader
+import torch
+import torch.nn as nn
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-dataset = ProteinLoader('/home/the_beast/mathisi/simon/attention/data')
+class TransformerModel(nn.Module):
 
-batch_size = 1
-num_workers = 1
-dataloader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    """
+    ntoken: the size of the vocabulary
+    emsize: embedding dimensions
+    nhid: the dimension of the feedforward network model in nn.TransformerEncoder
+    nlayers: the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+    nhead: the number of heads in the multiheadattention models
+    dropout: the dropout value
+    """
+    def __init__(self, ntoken, ninp, nhead, nhid, nlayers, dropout=0.5):
+        super(TransformerModel, self).__init__()
+        from torch.nn import TransformerEncoder, TransformerEncoderLayer
+        self.model_type = 'Transformer'
+        self.src_mask = None
+        self.pos_encoder = PositionalEncoding(ninp, dropout)
+        encoder_layers = TransformerEncoderLayer(ninp, nhead, nhid, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
+        self.encoder = nn.Embedding(ntoken, ninp)
+        self.ninp = ninp
+        self.decoder = nn.Linear(ninp, ntoken)
 
-for features, labels in dataset:
-    print(features)
-    print(labels)
-print(len(dataset))
+        self.init_weights()
 
-print('=' * 83)
+    def _generate_square_subsequent_mask(self, sz):
+        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+        return mask
 
-test_ds =  dataloader[:89, :, :]
-valid_ds =  dataloader[89:, :, :]
-print(test_ds.shape)
-print(valid_ds.shape)
+    def init_weights(self):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
 
-print('=' * 83)
+    def forward(self, src):
+        if self.src_mask is None or self.src_mask.size(0) != len(src):
+            device = src.device
+            mask = self._generate_square_subsequent_mask(len(src)).to(device)
+            self.src_mask = mask
 
-"""
-num_epochs = 10
-total_samples = len(dataset)
-n_iterations = math.ceil(total_samples / batch_size)
-print(total_samples, n_iterations)
+        src = self.encoder(src) * math.sqrt(self.ninp)
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(src, self.src_mask)
+        output = self.decoder(output)
+        return output
 
-for epoch in range(num_epochs):
-    for i, (inputs, labels) in enumerate(dataloader):
-        print(f"epoch {epoch+1}/{num_epochs}, step {i+1}/{n_iterations}, inputs {labels.shape}")
+class PositionalEncoding(nn.Module):
 
-"""
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+
+emsize = 200 # embedding dimension
+nhid = 200 # the dimension of the feedforward network model in nn.TransformerEncoder
+nlayers = 2 # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+nhead = 2 # the number of heads in the multiheadattention models
+dropout = 0.2 # the dropout value
+model = TransformerModel(emsize, nhead, nhid, nlayers, dropout).to(device)
+
+
+# num_epochs = 10
+# total_samples = len(dataset)
+# n_iterations = math.ceil(total_samples / batch_size)
+# print(total_samples, n_iterations)
+#
+# for epoch in range(num_epochs):
+#     for i, (inputs, labels) in enumerate(dataloader):
+#         print(f"epoch {epoch+1}/{num_epochs}, step {i+1}/{n_iterations}, inputs {labels.shape}")
